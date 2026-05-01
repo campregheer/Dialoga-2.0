c614b0d1545831202dd00bf1101f8ccb5c87e4e6
campregher07 <danieljunioor38@gmail.com>
2026-05-01 14:57:00 -0300

    refactor: update package.json scripts and enhance server.ts logging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,15 @@ import app from './app';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 
+console.log('🔹 Iniciando servidor...');
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 // Conecta ao banco de dados e inicia o servidor
 connectDB().then(() => {
+    console.log('🔹 Banco conectado');
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
   });
